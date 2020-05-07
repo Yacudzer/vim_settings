c099bb4a4a4f06c50ac053f8e37b3d2248b89682
Evgeniy Yakubov <yacudzer@mail.ru>
2020-05-07 09:15:36 +0000

    Обновлен init.sh; добавлнен README.md

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #/bin/bash
-echo "runtime vimrc.ini" > ~/.vimrc
-echo "installint autocomplete"
+configname="$(dirname $(realpath -e --relative-to=$HOME $0))/vimrc.ini";
+echo "runtime $configname" > ~/.vimrc;
+cd $(dirname $(realpath $0));
+git submodule update --init --recursive --force ;
 ./bundle/YouCompleteMe/install.py
 
