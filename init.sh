e82aca8faf3e8b59b05f9f24e7eea416de06b39a
Evgeniy Yakubov <yacudzer@mail.ru>
2020-05-07 08:45:51 +0000

    В init.sh добавлена процедура обновления подмодулей

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 configname="$(dirname $(realpath -e --relative-to=$HOME $0))/vimrc.ini";
 echo "runtime $configname" > ~/.vimrc;
+cd $(dirname $(realpath $0));
+git submodule update --init --recursive --force ;
 
